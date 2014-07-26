5fd15d5eee7750f4de372ea467d987b528e38448
radicalcakes <tbhuynh10@gmail.com>
2014-07-26 11:31:52 -0400

    added week 1 helpers

---
+++ week1-helpers/exercise-helpers/main.py
@@ -0,0 +1,23 @@
+from uf import UF, QuickFind
+
+if __name__ == '__main__':
+#create instances of the classes here and test with a text file 
+    with open('test.txt', 'r') as f:
+        n = int(f.readline())
+
+        qf = QuickFind(n)
+
+        for line in f.readlines():
+            l = line.split(' ')
+            p, q = (int(l[0]), int(l[1]))
+            if (qf.connected(p, q)):
+                continue
+            else:
+                qf.union(p, q)
+                print ' connections: %s %s' % (p, q)
+            
+        print '%s components' % qf.count()
+        
+        
+    
+    
---
+++ week1-helpers/exercise-helpers/uf.py
@@ -0,0 +1,47 @@
+#classes representing the different algorithms used in lecture 1
+#usage: from uf import QuickFind, ...
+
+#read 1.5 and view lectures 1.1 to understand how
+#this works
+
+class  UF(object):
+    
+    def __init__(self, n):
+        self.ct = n
+        self.id = range(n)
+
+    def count(self):
+        return self.ct
+
+    def find(self, p):
+        pass
+
+    def union(self, p, q):
+        pass
+    
+
+class QuickFind(UF):
+    
+    def connected(self, p, q):
+        return self.find(p) == self.find(q)
+
+    def find(self,  p):
+        return self.id[p]
+
+    def union(self, p, q):
+        #place p and q into the same component
+        idp = self.find(p)
+        idq = self.find(q)
+
+        if (not(idp == idq)):
+            for i in self.id:
+                print i
+                if(self.id[i] == idp):
+                    self.id[i] = idq
+        else:
+            return
+    
+        self.ct -= 1
+
+
+
